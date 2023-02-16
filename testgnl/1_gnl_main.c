d3b5435f9cb1ee11ea288a92894f5faf12c69de9
Gabriela <101965796+Gabriela-Tan@users.noreply.github.com>
2023-02-16 11:46:58 +0100

    Update 1_gnl_main.c
    2

--- testgnl/1_gnl_main.c
+++ testgnl/1_gnl_main.c
@@ -8,31 +8,27 @@ int main(void)
 	int fd[4];
 	size_t	i;
 
-	char s1[] = "testgnl0.txt";
-	char s2[] = "testgnl1.txt";
-	char s3[] = "testgnl2.txt";
-	char s4[] = "testgnl3.txt";
-	fd[0] = open(s1, O_RDONLY);
-	fd[1] = open(s2, O_RDONLY);
-	fd[2] = open(s3, O_RDONLY);
-	fd[3] = open(s4, O_RDONLY);
+	fd[0] = open("testgnl0.txt", O_RDONLY);
+	fd[1] = open("testgnl1.txt", O_RDONLY);
+	fd[2] = open("testgnl2.txt", O_RDONLY);
+	fd[3] = open("testgnl3.txt", O_RDONLY);
 	i = 0;
 	while (i < 4)
 	{
 		line = get_next_line(fd[i]);
-		printf("fd: %d | %s : %s\n", fd[i], s1, line);
+		printf("fd: %d | %s\n", fd[i], line);
 		free(line);
 
 		line = get_next_line(fd[i]);
-		printf("fd: %d | %s : %s\n", fd[i], s2, line);
+		printf("fd: %d | %s\n", fd[i], line);
 		free(line);
 
 		line = get_next_line(fd[i]);
-		printf("fd: %d | %s : %s\n", fd[i], s3, line);
+		printf("fd: %d | %s\n", fd[i], line);
 		free(line);
 
 		line = get_next_line(fd[i]);
-		printf("fd: %d | %s : %s\n", fd[i], s4, line);
+		printf("fd: %d | %s\n", fd[i], line);
 		free(line);
 
 		close(fd[i]);
